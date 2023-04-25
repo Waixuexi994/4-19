f7de04a79a5041e1fd395145e4fb387b25d89731
waixuexi <59311567+Waixuexi994@users.noreply.github.com>
2023-04-25 16:02:46 +0800

    Update system.ts

--- src/system.ts
+++ src/system.ts
@@ -11,10 +11,10 @@ export const defaultSetting = {
 }
 
 export const defaultMessage = `微信公众号：书虫创客
-- 网站以进行更改，现在网站不需要填写key即可使用，网站余额没了或者有其他问题添加书虫微信：SCFuYe003
+- 网站以进行更改，现在网站不需要填写key即可使用，网站余额没了或者有其他问题添加书虫微信：SCFuYe001
 - https://www.fakabang.com/links/3E277A6E
 - ↑↑↑，上面链接可以直接购买ChatGPT个人独享账号，自动发货，也可找书虫购买，免去手续费，手续费由平台收取
-- 现在国内镜像版版本，每分钟只能回答三次，想购买独立key或者ChatGPT官方账号也可以联系书虫微信：SCFuYe003
+- 现在国内镜像版版本，每分钟只能回答三次，想购买独立key或者ChatGPT官方账号也可以联系书虫微信：SCFuYe001
 - [[Shift]] + [[Enter]] 换行。开头输入 [[空格]] 跳出更多 预设。[[↑]] 可编辑最近一次提问。`
 
 export type Setting = typeof defaultSetting
